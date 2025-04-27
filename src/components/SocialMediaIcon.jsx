cb9436135877b0c843d957dc7d228a0a94f72c91
pedroowb <phsilvasantos7@gmail.com>
2025-04-26 23:13:05 -0300

    feat(components): cria o SocialMediaIcon

---
+++ src/components/SocialMediaIcon.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SocialMediaIcon = () => {
+  return (
+    <div className="d-flex gap-3">
+      <a href="#" style={{ color: "#1D2D44" }}>
+        <i className="fa-brands fa-facebook-f fa-2x"></i>
+      </a>
+      <a href="#" style={{ color: "#1D2D44" }}>
+        <i className="fa-brands fa-instagram fa-2x"></i>
+      </a>
+      <a href="#" style={{ color: "#1D2D44" }}>
+        <i className="fa-brands fa-whatsapp fa-2x"></i>
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcon;
